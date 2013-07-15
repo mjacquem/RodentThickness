f5fc7cdc7aad15e0d50b53cb159282b3de93b0cd
mjacquem <mjacquem@unc.edu>
2013-07-15 10:17:51 -0400

    Upload version of spharm-pdm

--- SuperBuild/External_spharm-pdm.cmake
+++ SuperBuild/External_spharm-pdm.cmake
@@ -19,7 +19,7 @@ set(proj ${extProjName}) #This local name
 
 ExternalProject_Add(spharm-pdm
   SVN_REPOSITORY "http://www.nitrc.org/svn/spharm-pdm"
-  SVN_REVISION -r "190"
+  SVN_REVISION -r "194"
   SVN_USERNAME slicerbot
   SVN_PASSWORD slicer
   SOURCE_DIR ${proj}
